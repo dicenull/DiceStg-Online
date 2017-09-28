85cf00e14fc2dd3ae97dccea4e9505b8d08e1d91
Eulerd <dicenull.dev@gmail.com>
2017-09-28 23:11:49 +0900

    [add] 位置を表す構造体の追加 二次元座標を表すためにDXLibやSystem.Drawingをusingするのがしっくりこなかったので単純なものを自作

---
+++ DiceStg-Online/Point.cs
@@ -0,0 +1,65 @@
+﻿namespace ShootingGameWithDice
+{
+    /// <summary>
+    /// 場所を表す構造体
+    /// </summary>
+    struct Point
+    {
+        /// <summary>
+        /// X座標の位置
+        /// </summary>
+        public int X { get; set; }
+
+        /// <summary>
+        /// Y座標の位置
+        /// </summary>
+        public int Y { get; set; }
+
+        /// <summary>
+        /// 座標の指定して初期化する
+        /// </summary>
+        /// <param name="x">x座標の初期値</param>
+        /// <param name="y">y座標の初期値</param>
+        public Point(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+        
+        public static Point operator-- (Point p)
+        {
+            p.X--;
+            p.Y--;
+
+            return p;
+        }
+
+        public static Point operator++ (Point p)
+        {
+            p.X++;
+            p.Y++;
+
+            return p;
+        }
+
+        public static Point operator+ (Point p1, Point p2)
+        {
+            return new Point(p1.X + p2.X, p1.Y + p2.Y);
+        }
+
+        public static Point operator+ (Point p, int v)
+        {
+            return new Point(p.X + v, p.Y + v);
+        }
+
+        public static Point operator- (Point p1, Point p2)
+        {
+            return new Point(p1.X - p2.X, p1.Y - p2.Y);
+        }
+
+        public static Point operator-(Point p, int v)
+        {
+            return new Point(p.X - v, p.Y - v);
+        }
+    }
+}
