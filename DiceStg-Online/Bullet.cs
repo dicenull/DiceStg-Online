f206562cb5a63dc9f500ea95a8a03f59f3484f1f
Eulerd <dicenull.dev@gmail.com>
2017-09-29 11:44:27 +0900

    Bulletクラスを追加, 位置と方向の実装

---
+++ DiceStg-Online/Bullet.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceStg_Online
+{
+    enum Direction
+    {
+        Up, Down, Left, Right
+    }
+    
+    class Bullet
+    {
+        public Point Position { get; private set; }
+        public bool IsEnable { get; private set; }
+
+        public Bullet(Player p, Direction dir)
+        {
+            Position = p.Position;
+            IsEnable = p.CanShooting;
+            Direction = dir;
+        }
+
+        public void Update()
+        {
+            switch(Direction)
+            {
+                case Direction.Left:
+                    Position.X--;
+                    break;
+                case Direction.Right:
+                    Position.X++;
+                    break;
+                case Direction.Up:
+                    Position.Y--;
+                    break;
+                case Direction.Down:
+                    Position.Y++;
+                    break;
+            }
+        }
+
+        private Direction Direction { get; }
+    }
+    
+}
