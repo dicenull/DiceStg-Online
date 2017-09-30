a229608de4dffbc33486eff234e752886a099463
Eulerd <dicenull.dev@gmail.com>
2017-09-30 22:59:56 +0900

    オフラインで動作させるためのプロジェクトを追加

---
+++ DiceStg-OnlineStandAlone/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DiceStg_Online.Core;
+
+namespace DiceStg_OnlineStandAlone
+{
+    public class Program
+    {
+        static void Main()
+        {
+            
+        }
+
+        private Game game;
+    }
+}
