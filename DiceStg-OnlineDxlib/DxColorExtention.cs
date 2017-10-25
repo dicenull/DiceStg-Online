6496c5bb3bb5fe6da6508c3faf76c949de2702cf
Eulerd <dicenull.dev@gmail.com>
2017-10-26 00:13:59 +0900

    ColorクラスからDxライブラリの色の値を取得できるようにした

---
+++ DiceStg-OnlineDxlib/DxColorExtention.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DxLibDLL;
+using DiceStg_Online.Core;
+
+namespace DiceStg_Online.Dxlib
+{
+    static class DxColorExtention
+    {
+        public static uint DxColor(this ColorState color)
+        {
+            return DX.GetColor(color.R, color.G, color.B);
+        }
+    }
+}
