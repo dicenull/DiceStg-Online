d1f605391eb29147c00380328af983c5447bab86
Eulerd <dicenull.dev@gmail.com>
2017-10-25 12:42:35 +0900

    使用していないプロパティを削除

--- DiceStg-Online/Field.cs
+++ DiceStg-Online/Field.cs
@@ -8,20 +8,38 @@ namespace DiceStg_Online.Core
 {
     public class Field
     {
+        /// <summary>
+        /// フィールドのデフォルトの幅
+        /// </summary>
         public static int DefaultWidth = 640;
+
+        /// <summary>
+        /// フィールドのデフォルトの高さ
+        /// </summary>
         public static int DefaultHeight = 480;
         
-        public Point StartPosition { get; }
+        /// <summary>
+        /// フィールドの幅
+        /// </summary>
         public int Width { get; }
+
+        /// <summary>
+        /// フィールドの高さ
+        /// </summary>
         public int Height { get; }
 
-        public Field() : this(new Point(0,0), DefaultWidth, DefaultHeight) { }
-        public Field(Point startPos) : this(startPos, DefaultWidth, DefaultHeight) { }
-        public Field(int w, int h) : this(new Point(0,0), w, h) { }
+        /// <summary>
+        /// デフォルト値を使用して初期化
+        /// </summary>
+        public Field() : this(DefaultWidth, DefaultHeight) { }
         
-        public Field(Point startPos, int w, int h)
+        /// <summary>
+        /// 幅と高さを指定して初期化
+        /// </summary>
+        /// <param name="w">幅</param>
+        /// <param name="h">高さ</param>
+        public Field(int w, int h)
         {
-            StartPosition = startPos;
             Width = w;
             Height = h;
         }
