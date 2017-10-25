725ceaa114d92e4dc3297ed370636e72c8513d94
Eulerd <dicenull.dev@gmail.com>
2017-10-26 00:12:36 +0900

    Playerを操作するClientクラスを追加

---
+++ DiceStg-OnlineStandAlone/Client.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DiceStg_Online.Core;
+
+namespace DiceStg_OnlineStandAlone
+{
+    public abstract class Client
+    {
+        public abstract ActionState Think(GameState state, int myPlayerNum);
+        
+    }
+}
